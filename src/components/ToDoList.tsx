827513a9af1c2a6c1b293ba05ce8a8740ae64d68
Leo-OShea <leo.oshea@wpengine.com>
2025-02-25 16:14:36 +0000

    cleaning up comments

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,13 +1,6 @@
 import React, { useState } from 'react';  
 import '../styles/ToDoList.css';  
 
-// Event Handling in React:
-// When you pass handleDeleteTask(index) directly, it will execute the function immediately when the component renders, 
-// rather than when the event occurs (e.g., a button click).
-
-// Arrow Function:
-// Using () => handleDeleteTask(index) creates a new function that calls handleDeleteTask(index) when the event occurs. 
-// This ensures that the function is called only when the event is triggered.
 type Task = { id: number, text: string, completed: boolean };
 
 var assignID = 0;
@@ -33,17 +26,13 @@ export default function ToDoList(){
         assignID++;
     }                       
 
-    // '_': The underscore is a convention to indicate that the parameter is not being used.
-    // The 'filter' method iterates over each element in the 'tasks' array and applies a 
-    // specified condition to determine which elements should be included in the new array.
-    // (The task is removed from the use state
+   // New array without deleted tasks
     function handleDeleteTask(id: number){
         const updatedTasks = tasks.filter((_, i) => i !== id);
         setTasks(updatedTasks);
     }
 
-    // if checkbox is checked, add a 'strikethrough' to the text
-    // if checkbox is unchecked, remove the 'strikethrough' on the text
+    // Toggle the completed status of a task
     function handleCheckboxClick(id: number){
         setTasks(tasks =>
             tasks.map(task =>
@@ -68,8 +57,6 @@ export default function ToDoList(){
                     <li key ={index}>
                         
                         <span className={task.completed ? 'completed' : 'text'} data-testid='task-test-id'>{task.text}</span>
-                        {/* might want to change 'input' to 'label' ? 
-                        W3Schools: "Always add the <label> tag for best accessibility practices!"*/}
                         <input type="checkbox" onChange={() => handleCheckboxClick(task.id)} checked={task.completed} ></input> 
                         <button className='delete-button' onClick={() => handleDeleteTask(index)}>X</button>
                     </li>
